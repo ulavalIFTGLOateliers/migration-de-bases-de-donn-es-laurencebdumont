81b5606b9d17a0cac3c14bf65e2ec1b384d45e13
laurencebdumont <laurence.b-dumont.1@ulaval.ca>
2024-03-14 10:39:17 -0400

    Commentaires deux fichier sql

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -3,22 +3,24 @@ CREATE TABLE IF NOT EXISTS band (     bandName varchar(50),
                                         creation int,
                                         genre varchar(50),
                                         PRIMARY KEY(bandName));
-
+-- Insertion des données dans la table band
 INSERT INTO band VALUES ("Crazy Duo", 2015, "rock"), ("Luna",2009,"classical"), ("Mysterio", 2019, "pop");
 
+-- Changement de nom de la table singer
 ALTER TABLE singer RENAME TO musician;
 
+-- Changement de nom de la colonne singerName
 ALTER TABLE musician
 CHANGE singerName musicianName varchar(50);
 
+-- Ajout de deux nouvelles colonnes dans la table musician
 ALTER TABLE musician
 ADD role varchar(50);
 
 ALTER TABLE musician
 ADD bandName varchar(50);
 
--- INSERT INTO musician VALUES ("Alina", "Darcy", "Boles", 32,"vocals","Crazy Duo"), ("Mysterio","Jessie","Chancey",23,"guitar","Mysterio"), ("Rainbow", "Sarah", "Derrick", 47,"percussion","Crazy Duo"), ("Luna", "Emily", "Seibold", 31,"piano","Luna");
-
+-- Insertion des données dans la nouvelle colonne Musician
 UPDATE musician
 SET role = CASE 
     WHEN musicianName = "Alina" THEN 'vocals'
--- db_scripts/rollback_1.sql
+++ db_scripts/rollback_1.sql
@@ -1,10 +1,14 @@
+-- Supprimer la table band
 DROP TABLE IF EXISTS band;
 
+-- Supprimer les deux colonnes de la table musician
 ALTER TABLE musician 
 DROP COLUMN role,
 DROP COLUMN bandName;
 
+-- Renommer la table musician
 ALTER TABLE musician RENAME TO singer;
 
+-- Renommer la colonne musicianName
 ALTER TABLE singer
 CHANGE musicianName singerName varchar(50);
